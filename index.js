6159b4300aecc8db5f9c3375f991d7d0dcb5bcbe
NikDoe <nikd0e@yandex.ru>
2023-03-17 13:36:41 +0300

    feat: add users route

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { usersRouter } from "./users/users.js";
 
 const port = 9000;
 const app = express();
@@ -6,6 +7,8 @@ const app = express();
 app.get("/", (req, res) => {
     res.send("hello there👍")
 })
+
+app.use("/users", usersRouter);
 
 app.listen(port, () => {
     console.log(`сервер запущен на порту ${port}`);
