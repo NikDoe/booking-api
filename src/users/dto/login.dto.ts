86c6005fa501a1269e280f9769343764b7ea9010
NikDoe <nikd0e@yandex.ru>
2023-03-24 14:01:13 +0300

    refactor: correct DTO fields validation

--- src/users/dto/login.dto.ts
+++ src/users/dto/login.dto.ts
@@ -1,9 +1,11 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDTO {
+	@IsNotEmpty({ message: 'Поле email обязательно для заполнения' })
 	@IsEmail({}, { message: 'Неверно указан email' })
 	email: string;
 
+	@IsNotEmpty({ message: 'Поле пароль обязательно для заполнения' })
 	@IsString({ message: 'укажите пароль' })
 	password: string;
 }
--- src/users/dto/register.dto.ts
+++ src/users/dto/register.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class RegisterDTO {
 	@IsNotEmpty({ message: 'Поле username обязательно для заполнения' })
@@ -8,6 +8,7 @@ export class RegisterDTO {
 	@IsEmail({}, { message: 'невалидный email' })
 	email: string;
 
-	@IsString({ message: 'укажите пароль' })
+	@IsNotEmpty({ message: 'Поле пароль обязательно для заполнения' })
+	@MinLength(4, { message: 'Пароль не должен быть кароче 4 символов' })
 	password: string;
 }
