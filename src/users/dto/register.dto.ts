39ee3cf2b3fe53fc14d29495ac540a103be3cab6
NikDoe <nikd0e@yandex.ru>
2023-03-29 19:40:21 +0300

    fix: right order of validate decorators

--- src/users/dto/register.dto.ts
+++ src/users/dto/register.dto.ts
@@ -1,14 +1,14 @@
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class RegisterDTO {
-	@IsNotEmpty({ message: 'Поле username обязательно для заполнения' })
+	@IsNotEmpty({ message: 'имя пользователя обязательно для заполнения' })
 	username: string;
 
-	@IsNotEmpty({ message: 'Поле username обязательно для заполнения' })
 	@IsEmail({}, { message: 'невалидный email' })
+	@IsNotEmpty({ message: 'email обязателен для заполнения' })
 	email: string;
 
-	@IsNotEmpty({ message: 'Поле пароль обязательно для заполнения' })
 	@MinLength(4, { message: 'Пароль не должен быть кароче 4 символов' })
+	@IsNotEmpty({ message: 'пароль обязателен для заполнения' })
 	password: string;
 }
