abe4e959a429a89b75ab03f54b8db74a6c841a4e
NikDoe <nikd0e@yandex.ru>
2023-03-31 17:59:10 +0300

    feat: start api migration from epress to nest

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,10 @@
-import { Container, ContainerModule, interfaces } from 'inversify';
-import { App } from './app';
-import { ExeptionFilter } from './errors/exeption.filter';
-import { IExeptionFilter } from './errors/exeption.filter.interface';
-import { ILogger } from './logger/logger.interface';
-import { LoggerSevice } from './logger/logger.service';
-import { TYPES } from './types';
-import { IUsersController } from './users/users.controller.interface';
-import { UsersController } from './users/users.controller';
-import { IUsersService } from './users/users.service.interface';
-import { UsersService } from './users/users.service';
-import { IConfigService } from './config/config.service.inteface';
-import { ConfigService } from './config/config.service';
-import { PrismaService } from '../database/prisma.service';
-import { IUsersRepository } from './users/users.repository.interface';
-import { UsersRepository } from './users/users.repository';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
-interface BootstrapReturn {
-	appContainer: Container;
-	app: App;
+async function bootstrap(): Promise<void> {
+	const PORT = process.env.PORT || 9999;
+	const app = await NestFactory.create(AppModule);
+	await app.listen(PORT, () => console.log(`сервер запущен на порту ${PORT}`));
 }
 
-export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
-	bind<App>(TYPES.Application).to(App);
-	bind<IConfigService>(TYPES.IconfigService).to(ConfigService).inSingletonScope();
-	bind<ILogger>(TYPES.ILogger).to(LoggerSevice).inSingletonScope();
-	bind<IUsersController>(TYPES.IUsersController).to(UsersController);
-	bind<IUsersService>(TYPES.IUsersService).to(UsersService);
-	bind<IExeptionFilter>(TYPES.IExeptionFilter).to(ExeptionFilter);
-	bind<IUsersRepository>(TYPES.IUsersRepository).to(UsersRepository).inSingletonScope();
-	bind<PrismaService>(TYPES.PrismaService).to(PrismaService).inSingletonScope();
-});
-
-const bootstrap = (): BootstrapReturn => {
-	const appContainer = new Container();
-	appContainer.load(appBindings);
-	const app = appContainer.get<App>(TYPES.Application);
-	app.init();
-	return { appContainer, app };
-};
-
-export const { appContainer, app } = bootstrap();
+bootstrap();
