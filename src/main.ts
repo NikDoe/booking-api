ac4def7a6c4a643d03a3b0b48f584412389dc325
NikDoe <nikd0e@yandex.ru>
2023-04-07 14:14:39 +0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap(): Promise<void> {
 	const PORT = process.env.PORT || 9999;
 	const app = await NestFactory.create(AppModule);
+	app.enableCors();
 	await app.listen(PORT);
 }
 
