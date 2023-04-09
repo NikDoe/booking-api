f2390585f04bb11bd870378fb7bea0ff145908e9
NikDoe <nikd0e@yandex.ru>
2023-04-09 10:59:38 +0300

    fix: throw error when login user by email not found

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -48,6 +48,11 @@ export class AuthService {
 
 	private async validateUser(loginDto: LoginDto): Promise<UserWithRoles> {
 		const existedUser = await this.usersRepository.getUserByEmail(loginDto.email);
+
+		if (!existedUser) {
+			throw new BadRequestException('неверный email или пароль');
+		}
+
 		const roles = await this.usersRepository.getUserRoles(loginDto.email);
 
 		const match = existedUser
