326d93f2a305ea4131fc91b6c63504ab5fca807b
NikDoe <nikd0e@yandex.ru>
2023-04-10 22:11:36 +0300

    feat: add origins to cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap(): Promise<void> {
 	const PORT = process.env.PORT || 9999;
 	const app = await NestFactory.create(AppModule);
-	app.enableCors();
+	app.enableCors({ origin: ['http://localhost:5173'], credentials: true });
 	app.use(cookieParser());
 	await app.listen(PORT);
 }
